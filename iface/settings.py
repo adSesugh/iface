2451d4f95cbf756f1e659441e4a27bcb0b269032
KeennessSolutions <sesughagbadu@yahoo.com>
2020-05-26 13:29:51 +0100

    enable staticcollect

--- iface/settings.py
+++ iface/settings.py
@@ -130,6 +130,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
